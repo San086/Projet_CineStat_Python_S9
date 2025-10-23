c2a81b1149f0a3c325765a5b2f8f4afcaa3c24b1
San086 <andreas.julien-caraguel@etu.unilasalle.fr>
2025-10-23 22:51:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,13 @@ st.text("Lise AYMONIN\nAndreas JULIEN-CARAGUEL\n")
 st.header("Problématique", divider="gray")
 st.link_button("Source de la donnée", "https://www.data.gouv.fr/datasets/frequentation-des-salles-de-cinema/")
 st.text("A quoi ressemblera la courbe des entrées pour les mois, années à venir ?\nQuelles sont les semaines et les mois de plus forte affluence dans l’année ?")
+
+
+fichier = '/content/Mise_en_forme_Frequentation_Salles_Cine.xlsx' #Collez votre chemin d'accès ici
+data = pd.read_excel(fichier, sheet_name ='Entrees_mois')
+
+
+st.header("Tableau de données (brut)", divider=True)
+data.insert(0, 'ID', range(1, 1 + len(data)))
+df = pd.DataFrame(data)
+df
